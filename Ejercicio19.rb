80460693321ed69b38f065b0f25f619929a8dbf0
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2022-10-10 12:38:08 -0300

    Agrego los ejercicios 18 y 19

---
+++ Ejercicio19.rb
@@ -0,0 +1,4 @@
+#Qué son los lazy enumerators? ¿Qué ventajas ves en ellos con respecto al uso de los enumeradores que no son lazy?
+
+## Es un metodo para que los enumeradores no sigan buscando una vez que encuentren lo que necesitaban
+##  Por ejemplo si busco las primeras 10 ocurrencias de la letra "A" en un archivo, una vez que los encuentre no va a buscar mas
---
+++ Práctica 2/Ejercicio18.rb
@@ -0,0 +1,8 @@
+fibonacci = Enumerator.new do |caller|
+    i1, i2 = 1, 1
+    loop do
+      caller.yield i1
+      i1, i2 = i2, i1+i2
+    end
+  end
+  6.times { puts fibonacci.next }
